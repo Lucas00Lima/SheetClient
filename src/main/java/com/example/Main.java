587399a14aa35f44b899f3cc32ce7df9cb2b6308
Lucas00Lima <lukinha-lenta@hotmail.com>
2023-06-15 00:18:50 -0300

    Projeto Finalizado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,10 +7,12 @@ import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -19,29 +21,21 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-/*						Quando for um cliente Pessoa Fisica
-													type1 = 1
-													gender = 1 (masculino) e 2 (feminino)
-													id_doc_number2
-						Quando for um cliente Pessoa Juridica
-													type1 = 2
-													id_doc_number3 = CNPJ
-													id_doc_number4 = IE
-*/
 public class Main {
 	public static void main(String[] args) {
-		/*
-		 * JFileChooser fileChooser = new JFileChooser(); int result =
-		 * fileChooser.showOpenDialog(null); String filePath = null; if (result ==
-		 * JFileChooser.APPROVE_OPTION) { filePath =
-		 * fileChooser.getSelectedFile().getAbsolutePath();
-		 * JOptionPane.showMessageDialog(null, "Arquivo Selecionado" + filePath); }
-		 */
-		String filePath = "C:\\Users\\lukin\\OneDrive\\Área de Trabalho\\planilha\\cliente.xlsx";
-		String username = "root"; /* JOptionPane.showInputDialog("Insira o Usuario do DB"); */
-		String password = "@soma+"; /* JOptionPane.showInputDialog("Insira a senha do DB"); */
-		String db = "db000"; /* JOptionPane.showInputDialog("Insira o banco que deseja fazer a Query"); */
-		String table = "client"; /* JOptionPane.showInputDialog("Insira a tabela"); */
+
+		JFileChooser fileChooser = new JFileChooser();
+		int result = fileChooser.showOpenDialog(null);
+		String filePath = null;
+		if (result == JFileChooser.APPROVE_OPTION) {
+			filePath = fileChooser.getSelectedFile().getAbsolutePath();
+			JOptionPane.showMessageDialog(null, "Arquivo Selecionado" + filePath);
+		}
+
+		String username = JOptionPane.showInputDialog("Insira o Usuario do DB");
+		String password = JOptionPane.showInputDialog("Insira a senha do DB");
+		String db = JOptionPane.showInputDialog("Insira o banco que deseja fazer a Query");
+		String table = JOptionPane.showInputDialog("Insira a tabela");
 		String url = "jdbc:mysql://localhost:3306/" + db;
 		String defaultValue = "";
 		try (Connection connection = DriverManager.getConnection(url, username, password)) {
@@ -49,58 +43,16 @@ public class Main {
 			Workbook workbook = WorkbookFactory.create(fileInputStream);
 			Sheet sheet = workbook.getSheetAt(0);
 			DataFormatter dataFormatter = new DataFormatter();
-			StringBuilder insertQuery = new StringBuilder("INSERT INTO " + table
-					+ " (id,name,type1,id_doc_number2,id_doc_number3,id_doc_number4,cell_phone,cell_phone2,gender,email,birthday,register");
-			StringBuilder valuePlaceholders = new StringBuilder(" VALUES (?,?,?,?,?,?,?,?,?,?,?,?");
+			String insertQuery = "INSERT INTO client (id, name, type1, id_doc_number2, id_doc_number3, id_doc_number4, cell_phone, cell_phone2, gender, email, birthday, register, "
+					+ "occupation, phone, phone2, state_id, city_id, street_name, street_number, zipcode, neighborhood_id, complement, id_doc_number, id_doc_number5, company_id, "
+					+ "notes, locked, coordinates, expiration_day, expiration_month_offset, client_credit_limit, company_credit_limit, value1, payments, employee_id, active, value2, value3, value4, "
+					+ "external_id, badge, external_system_id, is_trainee, doc_name, owner_name, type2, send_notifications) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+					+ "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			List<String> defaultValues = new ArrayList<>();
 			DatabaseMetaData metaData = (DatabaseMetaData) connection.getMetaData();
 			ResultSet resultSet = metaData.getColumns(null, null, table, null);
 			SimpleDateFormat inputDateFormat = new SimpleDateFormat("dd.MM.yyyy\\yyyy\\yyyy\\yyyy");
 			SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			int totalColumnsInDataBase = 13;
-			while (resultSet.next()) {
-				String columnName = resultSet.getString("COLUMN_NAME").toLowerCase();
-				if (!columnName.equals("id") && !columnName.equals("name") && !columnName.equals("type1")
-						&& !columnName.equals("id_doc_number2") && !columnName.equals("id_doc_number3")
-						&& !columnName.equals("id_doc_number4") && !columnName.equals("cell_phone")
-						&& !columnName.equals("cell_phone2") && !columnName.equals("gender")
-						&& !columnName.equals("email") && !columnName.equals("birthday")
-						&& !columnName.equals("register")) {
-					if (totalColumnsInDataBase > 0) {
-						insertQuery.append(",");
-						valuePlaceholders.append(",");
-					}
-					insertQuery.append(columnName);
-					valuePlaceholders.append("?");
-					defaultValues.add(defaultValue);
-					totalColumnsInDataBase++;
-				}
-				if (columnName.equals("is_trainee")) {
-					break;
-				}
-			}
-			resultSet.close();
-			insertQuery.append(")");
-			valuePlaceholders.append(")");
-			insertQuery.append(valuePlaceholders);
-
-			Statement statement = connection.createStatement();
-			String alterTableQuery = "ALTER TABLE " + table;
-			String columnsQuery = "SHOW COLUMNS FROM " + table;
-			statement.execute(columnsQuery);
-			var result = statement.getResultSet();
-			while (result.next()) {
-				String columnNameString = result.getString("Field");
-				String columnType = result.getString("Type");
-				if (!columnNameString.equalsIgnoreCase("id")) {
-					String modifyColumnQuery = alterTableQuery + " MODIFY " + columnNameString + " " + columnType
-							+ " NULL";
-					statement.addBatch(modifyColumnQuery);
-				}
-			}
-			String alterTable = "ALTER TABLE client MODIFY COLUMN id_doc_number2 VARCHAR(50);";
-			statement.addBatch(alterTable);
-			statement.executeBatch();
 			int rowIndex;
 			int totalLinhasInseridas = 0;
 			for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
@@ -146,7 +98,40 @@ public class Main {
 				} else {
 					typeResult = 2;
 				}
-				PreparedStatement preparedStatement = connection.prepareStatement(insertQuery.toString());
+
+				String valueOccupation = "";
+				String valuePhone = "";
+				String valuePhone2 = "";
+				Integer valueStateId = 0;
+				Integer valueCityId = 0;
+				Integer valueStreetName = 0;
+				Integer valueStreetNumber = 0;
+				String valueZipcode = "";
+				Integer valueNeighborhoodId = 0;
+				String valueComplement = "";
+				String valueIdDocNumber = "";
+				String valueIdDocNumber5 = "";
+				Integer valueCompanyId = 0;
+				String valueNotes = "";
+				Integer valueLocked = 0;
+				String valueCoordinates = "";
+				Integer valueExpirationDay = 0;
+				Integer valueExpirationMonthOffset = 0;
+				Integer valueClientCreditLimit = 0;
+				Integer valueCompanyCreditLimit = 0;
+				Integer valueValue1 = 0;
+				String valuePayments = "";
+				Integer valueEmployeeId = 0;
+				Integer valueActive = 0;
+				Integer valueValue2 = 0;
+				Integer valueValue3 = 0;
+				Integer valueValue4 = 0;
+				Integer valueExternalId = 0;
+				String valueBadge = "";
+				String valueExternalSystemId = "";
+				Integer valueIsTrainee = 0;
+
+				PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
 				preparedStatement.setInt(1, idDouble);
 				preparedStatement.setString(2, nameValue);
 				preparedStatement.setInt(3, typeResult);
@@ -159,35 +144,54 @@ public class Main {
 				preparedStatement.setString(10, emailValue);
 				preparedStatement.setString(11, formattedBirthday);
 				preparedStatement.setString(12, formattedRegister);
-				for (int j = 0; j < defaultValues.size(); j++) {
-					String value = defaultValues.get(j);
-					if (value.isEmpty()) {
-						preparedStatement.setNull(j + 13, java.sql.Types.NULL);
-					} else {
-						preparedStatement.setString(j + 13, "");
-					}
-				}
+
+				preparedStatement.setString(13, valueOccupation);
+				preparedStatement.setString(14, valuePhone);
+				preparedStatement.setString(15, valuePhone2);
+				preparedStatement.setInt(16, valueStateId);
+				preparedStatement.setInt(17, valueCityId);
+				preparedStatement.setInt(18, valueStreetName);
+				preparedStatement.setInt(19, valueStreetNumber);
+				preparedStatement.setString(20, valueZipcode);
+				preparedStatement.setInt(21, valueNeighborhoodId);
+				preparedStatement.setString(22, valueComplement);
+				preparedStatement.setString(23, valueIdDocNumber);
+				preparedStatement.setString(24, valueIdDocNumber5);
+				preparedStatement.setInt(25, valueCompanyId);
+				preparedStatement.setString(26, valueNotes);
+				preparedStatement.setInt(27, valueLocked);
+				preparedStatement.setString(28, valueCoordinates);
+				preparedStatement.setInt(29, valueExpirationDay);
+				preparedStatement.setInt(30, valueExpirationMonthOffset);
+				preparedStatement.setInt(31, valueClientCreditLimit);
+				preparedStatement.setInt(32, valueCompanyCreditLimit);
+				preparedStatement.setInt(33, valueValue1);
+				preparedStatement.setString(34, valuePayments);
+				preparedStatement.setInt(35, valueEmployeeId);
+				preparedStatement.setInt(36, valueActive);
+				preparedStatement.setInt(37, valueValue2);
+				preparedStatement.setInt(38, valueValue3);
+				preparedStatement.setInt(39, valueValue4);
+				preparedStatement.setInt(40, valueExternalId);
+				preparedStatement.setString(41, valueBadge);
+				preparedStatement.setString(42, valueExternalSystemId);
+				preparedStatement.setInt(43, valueIsTrainee);
+				preparedStatement.setString(44, "");
+				preparedStatement.setString(45, "");
+				preparedStatement.setInt(46, 0);
+				preparedStatement.setInt(47, 0);
+
 				preparedStatement.execute();
 				preparedStatement.close();
 				totalLinhasInseridas++;
 			}
-			ResultSet resultSet1 = metaData.getColumns(null, null, table, null);
-			statement.execute(columnsQuery);
-			while (resultSet1.next()) {
-				String columnNameString = resultSet1.getString("Field");
-				String columnType = resultSet1.getString("Type");
-				if (!columnNameString.equalsIgnoreCase("id")) {
-					String modifyColumnQuery = alterTableQuery + " MODIFY " + columnNameString + " " + columnType
-							+ " NOT NULL";
-					statement.addBatch(modifyColumnQuery);
-					System.out.println(modifyColumnQuery.toString());
-				}
-				statement.executeBatch();
-				statement.close();
-			}
 			System.out.println("Row affected = " + totalLinhasInseridas);
 			connection.close();
-		} catch (Exception e) {
+		}
+
+		catch (
+
+		Exception e) {
 			e.printStackTrace();
 		}
 	}
